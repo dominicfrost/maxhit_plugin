439eb0206b6375e1935c094ed5940f640f5bacf5
Dominic Frost <dominic.j.frost@gmail.com>
2021-08-24 20:08:05 -0700

    remove final unused imports.

--- src/main/java/com/github/dominicfrost/maxhit_plugin/MaxHitConfig.java
+++ src/main/java/com/github/dominicfrost/maxhit_plugin/MaxHitConfig.java
@@ -2,7 +2,6 @@ package com.github.dominicfrost.maxhit_plugin;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("maxhit")
 public interface MaxHitConfig extends Config {}
